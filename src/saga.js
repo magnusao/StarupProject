0baa3911e4a552a0fb2dbbd2fe5019090c95984d
Alfredvc <alcl@netlight.com>
2016-06-13 10:44:10 +0200

    Fixed not a function error

--- src/saga.js
+++ src/saga.js
@@ -1,15 +1,19 @@
 import {fetchImages} from './instagram';
-import {put, take} from 'redux-saga/effects';
+import {put, take, call, fork} from 'redux-saga/effects';
 
 export function* loadImages() {
-  const images = yield fetchImages();
-  yield put({type: 'IMAGES_LOADED', images})
-  yield put({type: 'IMAGE_SELECTED', image: images[0]})
+  try {
+    const images = yield call(fetchImages);
+    yield put({type: 'IMAGES_LOADED', images})
+    yield put({type: 'IMAGE_SELECTED', image: images[0]})
+  } catch(error) {
+    yield put({type: 'IMAGE_LOAD_FAILURE', error})
+  }
 }
 
-export function* watchForLoadImages(){
-	while(true){
-		yield take('LOAD_IMAGES');
-		yield loadImages();
-	}
+export function* watchForLoadImages() {
+  while(true) {
+    yield take('LOAD_IMAGES');
+    yield call(loadImages);
+  }
 }

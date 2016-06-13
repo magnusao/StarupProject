6c6c5329af907908767ebf1840f2caf441a21671
Lasse Drevland <ladr@osl-m-ladr.sto.netlight.se>
2016-06-13 13:09:01 +0200

    Netlight USER_ID included

--- src/instagram.js
+++ src/instagram.js
@@ -1,10 +1,8 @@
 const API_ACCESS_TOKEN = '398360010.0387370.5e1bb9daf23b431982b33cd4180f454a';
-const API_USER_ID = '30632597'
-const API_ENDPOINT =  'https://api.instagram.com/v1/users/${API_USER_ID}/media/recent/?access_token=${API_ACCESS_TOKEN}';
-
+const API_USER_ID = '577141516'
+const API_ENDPOINT =  `https://api.instagram.com/v1/users/${API_USER_ID}/media/recent/?access_token=${API_ACCESS_TOKEN}`;
 export const fetchImages = () => {
-	const url = (({API_ACCESS_TOKEN, API_USER_ID}) => API_ENDPOINT)
-	console.log(({farm, server, id, secret}) => 'https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg')
+	const url = (API_ENDPOINT)
   return fetch(url).then(function (response) {
   		console.log(response)
   	  return response.json().then(function (json) {
